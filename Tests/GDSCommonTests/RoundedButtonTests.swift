1cd162843a6e09b8963f7b173f5bcdf380585d43
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2023-09-13 14:37:26 +0100

    chore: adding tests for RoundedButton loading state

--- Tests/GDSCommonTests/RoundedButtonTests.swift
+++ Tests/GDSCommonTests/RoundedButtonTests.swift
@@ -19,7 +19,7 @@ final class GDSButtonTests: XCTestCase {
 }
 
 extension GDSButtonTests {
-    func test_buttonAccessibilityBackground() {
+    func test_buttonAccessibilityBackground() throws {
         sut.accessibilityElementDidBecomeFocused()
         XCTAssertEqual(sut.backgroundColor, .gdsYellow)
         XCTAssertEqual(sut.currentTitleColor, .black)
@@ -27,5 +27,26 @@ extension GDSButtonTests {
         sut.accessibilityElementDidLoseFocus()
         XCTAssertEqual(sut.backgroundColor, .gdsGreen)
         XCTAssertEqual(sut.currentTitleColor, .white)
+    }
+    
+    func test_initialLoadingState() throws {
+        XCTAssertFalse(sut.isLoading)
+        XCTAssertFalse(sut.isTitleHidden)
+        XCTAssertTrue(sut.isEnabled)
+    }
+    
+    func test_tappedLoadingState() throws {
+        sut.isLoading = true
+        XCTAssertTrue(sut.isLoading)
+        XCTAssertTrue(sut.isTitleHidden)
+        XCTAssertFalse(sut.isEnabled)
+    }
+    
+    func test_tappedTwiceLoadingState() throws {
+        sut.isLoading = true
+        sut.isLoading = false
+        XCTAssertFalse(sut.isLoading)
+        XCTAssertFalse(sut.isTitleHidden)
+        XCTAssertTrue(sut.isEnabled)
     }
 }
