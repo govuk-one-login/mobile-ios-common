50dbd03c3dcccbabce3f4da77cdb7106da532f70
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2023-10-02 17:47:15 +0100

    chore: adding test for viewDidAppear

--- GDSCommon-Demo/GDSCommon-DemoTests/WelcomeViewControllerTests.swift
+++ GDSCommon-Demo/GDSCommon-DemoTests/WelcomeViewControllerTests.swift
@@ -4,12 +4,14 @@ import XCTest
 final class WelcomeViewControllerTests: XCTestCase {
     var viewModel: WelcomeViewModel!
     var sut: WelcomeViewController!
-    var didTapButton = false
+    var viewDidAppear = false
     
     override func setUp() {
         super.setUp()
         
-        viewModel = TestViewModel()
+        viewModel = TestViewModel() {
+            self.viewDidAppear = true
+        }
         sut = WelcomeViewController(viewModel: viewModel)
     }
     
@@ -22,16 +24,19 @@ final class WelcomeViewControllerTests: XCTestCase {
 }
 
 private struct TestViewModel: WelcomeViewModel {
-    var image: UIImage = UIImage()
-    var title: GDSLocalisedString = "welcome screen title"
-    var body: GDSLocalisedString = "welcome screen body"
-    var welcomeButtonViewModel: ButtonViewModel
+    let image: UIImage = UIImage()
+    let title: GDSLocalisedString = "welcome screen title"
+    let body: GDSLocalisedString = "welcome screen body"
+    let welcomeButtonViewModel: ButtonViewModel = MockButtonViewModel(title: "welcome screen button title", action: { })
+    let action: () -> Void
     
-    init() {
-        welcomeButtonViewModel = MockButtonViewModel(title: "welcome screen button title", action: { })
+    init(action: @escaping () -> Void) {
+        self.action = action
     }
     
-    func didAppear() { }
+    func didAppear() {
+        action()
+    }
 }
 
 extension WelcomeViewControllerTests {
@@ -43,6 +48,13 @@ extension WelcomeViewControllerTests {
         try XCTAssertEqual(sut.bodyLabel.text, "welcome screen body")
         XCTAssertFalse(try sut.bodyLabel.accessibilityTraits.contains(.header))
         try XCTAssertEqual(sut.welcomeButton.title(for: .normal), "welcome screen button title")
+    }
+    
+    func test_viewDidAppear() throws {
+        XCTAssertFalse(viewDidAppear)
+        sut.beginAppearanceTransition(true, animated: false)
+        sut.endAppearanceTransition()
+        XCTAssertTrue(viewDidAppear)
     }
 }
 
