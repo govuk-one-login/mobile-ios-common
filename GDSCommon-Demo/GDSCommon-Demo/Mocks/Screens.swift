493bbae504bc9a97227d8bf745b346e2eae641fc
Ben McKillop <ben.mckillop@digital.cabinet-office.gov.uk>
2023-10-04 11:28:01 +0100

    doc: fixing some swiftlint violations

--- GDSCommon-Demo/GDSCommon-Demo/Mocks/Screens.swift
+++ GDSCommon-Demo/GDSCommon-Demo/Mocks/Screens.swift
@@ -6,9 +6,11 @@ import UIKit
 /// To add a new `Screens` attribute
 /// - add a new case for the view
 /// - declare if it should be modal in presentation
-/// - create a static var for the UIViewController to be presented 
+/// - create a static var for the UIViewController to be presented
 ///
-/// - Returns: The view name as a `String`, isModal as a `Bool` to determin if it should be presented modally, and the view as a `UIViewController` to push/present on the navigation stack.
+/// - Returns: - The view name as a `String`
+///             isModal as a `Bool` to determine if it should be presented modally
+///             the view as a `UIViewController` to push/present on the navigation stack.
 enum Screens: CaseIterable {
     case gdsInstructions
     case gdsInstructionsWithImage
@@ -34,20 +36,18 @@ enum Screens: CaseIterable {
         }
     }
     
+    private var mockButtonViewModel: ButtonViewModel {
+        MockButtonViewModel(title: "Action Button", shouldLoadOnTap: false, action: {})
+    }
+    
     var view: UIViewController {
         switch self {
         case .gdsInstructions:
-            let viewModel = MockGDSInstructionsViewModel(buttonViewModel: MockButtonViewModel(title: "Action Button",
-                                                                                              shouldLoadOnTap: false,
-                                                                                              action: {}))
+            let viewModel = MockGDSInstructionsViewModel(buttonViewModel: mockButtonViewModel)
             return GDSInstructionsViewController(viewModel: viewModel)
         case .gdsInstructionsWithImage:
-            let viewModel = MockInstructionWithImageViewModel(warningButtonViewModel: MockButtonViewModel(title: "Warning Button",
-                                                                                                          shouldLoadOnTap: false,
-                                                                                                          action: {}),
-                                                              primaryButtonViewModel: MockButtonViewModel(title: "Action Button",
-                                                                                                          shouldLoadOnTap: false,
-                                                                                                          action: {}))
+            let viewModel = MockInstructionWithImageViewModel(warningButtonViewModel: mockButtonViewModel,
+                                                              primaryButtonViewModel: mockButtonViewModel)
             return InstructionsWithImageViewController(viewModel: viewModel)
         case .gdsModalInfoView:
             let viewModel = MockModalInfoViewModel()
