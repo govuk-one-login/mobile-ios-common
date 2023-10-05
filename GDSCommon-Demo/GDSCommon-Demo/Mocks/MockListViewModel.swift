57f3bd4892a54a841cfff361a2d24b88799cc377
Robin Phillips <rlphillips@deloitte.co.uk>
2023-10-05 12:16:30 +0100

    chore: indentation and adjusting button view model initialisation in MockListViewModel

--- GDSCommon-Demo/GDSCommon-Demo/Mocks/MockListViewModel.swift
+++ GDSCommon-Demo/GDSCommon-Demo/Mocks/MockListViewModel.swift
@@ -5,14 +5,13 @@ struct MockListViewModel: ListOptionsViewModel {
     let body: String? = "This is the optional body label. If the view model property is `nil` then the label is hidden."
     let listRows: [GDSLocalisedString] = ["Table view list item 1", "Table view list item two", "Table view list item 3", "Table view list item IV"]
     let listFooter: String? = "Optional footer. Configure it on the view model in a similar way as the `body` property. The right bar button works the same way."
-    let buttonViewModel: ButtonViewModel = MockButtonViewModel(title: "Action button",
-                                                                      action: { print("button was tapped") })
+    let buttonViewModel: ButtonViewModel
     let resultAction: (GDSLocalisedString) -> Void
     let rightBarButtonTitle: GDSLocalisedString? = "Right bar button"
-
+    
     let screenView: () -> Void
     let dismissAction: () -> Void
-
+    
     func didDismiss() {
         dismissAction()
     }
@@ -23,12 +22,15 @@ struct MockListViewModel: ListOptionsViewModel {
     
     init(resultAction: ((GDSLocalisedString) -> Void)? = nil,
          screenView: (() -> Void)? = nil,
-         dismissAction: (() -> Void)? = nil
-    ) {
+         dismissAction: (() -> Void)? = nil,
+         buttonAction: (() -> Void)? = nil) {
         self.resultAction = resultAction ?? { _ in
             // the resultAction shouldn't be nil
         }
         self.screenView = screenView ?? {}
         self.dismissAction = dismissAction ?? {}
+        
+        buttonViewModel = MockButtonViewModel(title: "Action button",
+                                              action: dismissAction ?? {})
     }
 }
