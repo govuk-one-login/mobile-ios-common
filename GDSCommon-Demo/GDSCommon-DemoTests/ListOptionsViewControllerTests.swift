40a16aa95a6e409ff057b288d050d227dff448a4
Robin Phillips <rlphillips@deloitte.co.uk>
2023-10-05 09:52:11 +0100

    chore: linting issue

--- GDSCommon-Demo/GDSCommon-DemoTests/ListOptionsViewControllerTests.swift
+++ GDSCommon-Demo/GDSCommon-DemoTests/ListOptionsViewControllerTests.swift
@@ -79,7 +79,7 @@ extension ListOptionsViewControllerTests {
         
         XCTAssertFalse(didDismiss)
         
-        let _ = sut.navigationItem.rightBarButtonItem?.target?.perform(sut.navigationItem.rightBarButtonItem?.action)
+        _ = sut.navigationItem.rightBarButtonItem?.target?.perform(sut.navigationItem.rightBarButtonItem?.action)
         XCTAssertTrue(didDismiss)
     }
     
