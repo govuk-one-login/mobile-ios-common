64f78663fb1ebe4172293c4e130fe6e217d52f46
Ben McKillop <ben.mckillop@digital.cabinet-office.gov.uk>
2023-10-05 12:09:32 +0100

    feat: Adding reusable QR Code scanner

--- Sources/GDSCommon/UI/UIKit/SharedViews/DialogView/DialogPresenter.swift
+++ Sources/GDSCommon/UI/UIKit/SharedViews/DialogView/DialogPresenter.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 public protocol DialogPresenter {
-    func present(onView view: UIView) async
+    func present(onView view: UIView, shouldLoad: Bool?, title: String?) async
     func updateState(isLoading: Bool, newTitle: String, view: UIView) async
     func remove(view: UIView) async
 }
