b98490d0d2f2eb9f57be6867374f34fdd93bc7b3
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2023-10-06 14:30:42 +0100

    fix: removing force unwrap for UIImage

--- GDSCommon-Demo/GDSCommon-Demo/Mocks/MockIntroViewModel.swift
+++ GDSCommon-Demo/GDSCommon-Demo/Mocks/MockIntroViewModel.swift
@@ -2,7 +2,7 @@ import GDSCommon
 import UIKit
 
 struct MockIntroViewModel: IntroViewModel {
-    var image: UIImage = UIImage(named: "badge")!
+    var image: UIImage = UIImage(named: "badge") ?? UIImage()
     var title: GDSLocalisedString = "This is a Welcome Screen"
     var body: GDSLocalisedString = "This is the body where we can give a brief description of the app"
     var introButtonViewModel: ButtonViewModel
