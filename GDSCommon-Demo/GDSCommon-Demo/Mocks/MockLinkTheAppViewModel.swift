15b5a1d3741fe9b68c82042d726d0951b70b3f03
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2023-10-13 20:16:09 +0100

    chore: setting up view assembly in view model

--- GDSCommon-Demo/GDSCommon-Demo/Mocks/MockLinkTheAppViewModel.swift
+++ GDSCommon-Demo/GDSCommon-Demo/Mocks/MockLinkTheAppViewModel.swift
@@ -8,9 +8,25 @@ struct MockLinkTheAppViewModel: LinkTheAppViewModel {
     var contentViews: [UIStackView]
     
     init() {
+        // MARK: Labels
         let firstLabel = UILabel()
         firstLabel.text = "Example Text"
-        let firstStackView = UIStackView(arrangedSubviews: [firstLabel])
-        contentViews = [firstStackView]
+        let secondLabel = UILabel()
+        secondLabel.text = "Second Example Text"
+        let labels = [firstLabel, secondLabel]
+        // MARK: Label Operations
+        labels.forEach { view in
+            
+        }
+        
+        // MARK: StackViews
+        let firstStackView = UIStackView(arrangedSubviews: labels)
+        let stackViews = [firstStackView]
+        // MARK: StackView Operations
+        stackViews.forEach { view in
+            view.axis = .vertical
+        }
+        
+        contentViews = stackViews
     }
 }
