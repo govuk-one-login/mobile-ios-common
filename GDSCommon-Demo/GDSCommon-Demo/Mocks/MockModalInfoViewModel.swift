304fe468cfc1842a7131dc9b232332fb95bd4e98
Mathieu Amouny <mathieu.amouny@digital.cabinet-office.gov.uk>
2023-10-16 16:17:37 +0100

    fix: swiftlint line length error

--- GDSCommon-Demo/GDSCommon-Demo/Mocks/MockModalInfoViewModel.swift
+++ GDSCommon-Demo/GDSCommon-Demo/Mocks/MockModalInfoViewModel.swift
@@ -13,7 +13,9 @@ class MockModalInfoViewModel: ModalInfoViewModel {
 class MockAttributedModalInfoViewModel: ModalInfoViewModel {
     var title: GDSLocalisedString = "This is the modal view"
     var body: GDSLocalisedString?
-    var attributedBody: GDSAttributedString? = GDSAttributedString(localisedString: "We can use this if we want the user to complete an action", attributes: [.font: UIFont.bodyBold], stringToAttribute: "We can use this if")
+    var attributedBody: GDSAttributedString? = GDSAttributedString(localisedString: "We can use this if we want the user to complete an action",
+                                                                   attributes: [.font: UIFont.bodyBold],
+                                                                   stringToAttribute: "We can use this if")
     var rightBarButtonTitle: GDSLocalisedString = "Close"
     func didAppear() {}
     func didDismiss() {}
