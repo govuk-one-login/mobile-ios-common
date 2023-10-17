8d3a4386f639736930cefc00d6192195184f7c0d
Mathieu Amouny <mathieu.amouny@digital.cabinet-office.gov.uk>
2023-10-17 13:03:40 +0100

    fix: swiftlint errors

--- GDSCommon-Demo/GDSCommon-Demo/Mocks/MockModalInfoViewModel.swift
+++ GDSCommon-Demo/GDSCommon-Demo/Mocks/MockModalInfoViewModel.swift
@@ -12,7 +12,7 @@ class MockModalInfoViewModel: ModalInfoViewModel {
 
 class MockAttributedModalInfoViewModel: ModalInfoViewModel {
     var title: GDSLocalisedString = "This is the modal view"
-    var body: GDSLocalisedString = .init(stringLiteral: "We can use this if we want the user to complete an action", attributes: [("We can use this", [.font: UIFont.bodyBold])])    
+    var body: GDSLocalisedString = .init(stringLiteral: "We can use this if we want the user to complete an action", attributes: [("We can use this", [.font: UIFont.bodyBold])])
     var rightBarButtonTitle: GDSLocalisedString = "Close"
     func didAppear() {}
     func didDismiss() {}
