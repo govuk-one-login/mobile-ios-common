bc32914f3ccc510ca4f48ac79123e06f1c230c24
Mathieu Amouny <mathieu.amouny@digital.cabinet-office.gov.uk>
2023-10-18 14:34:18 +0100

    docs: remove typo

--- Sources/GDSCommon/UI/UIKit/SharedViews/Patterns/ModalInfoView/ModalInfoViewModel.swift
+++ Sources/GDSCommon/UI/UIKit/SharedViews/Patterns/ModalInfoView/ModalInfoViewModel.swift
@@ -3,7 +3,7 @@ import UIKit
 /// View model for the `ModalInfoViewController`
 /// - `title` type is ``GDSLocalisedString``
 /// - `body` type is ``GDSLocalisedString``
-/// -
+///
 /// Additionally there are protocol methods for `didAppear()` and `didDismiss()` which should be used
 /// for any code that needs to be performed when the screen appears or is dismissed.
 /// For example, this might include tracking an analytics screen view, but it could be used
