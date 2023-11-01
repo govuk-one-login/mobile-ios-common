2257bab9246eb5b5c27e0dfd3c4fa8b9fd65dd21
Ben McKillop <ben.mckillop@digital.cabinet-office.gov.uk>
2023-11-01 10:48:14 +0000

    fix: making tests build

--- Tests/GDSCommonTests/ComponentTests/Mocks/MockButtonViewModel.swift
+++ Tests/GDSCommonTests/ComponentTests/Mocks/MockButtonViewModel.swift
@@ -2,11 +2,11 @@ import GDSCommon
 
 internal struct MockButtonViewModel: ButtonViewModel {
     let title: GDSLocalisedString
-    let icon: String?
+    let icon: ButtonIconViewModel?
     let shouldLoadOnTap: Bool
     let action: () -> Void
     
-    init(title: GDSLocalisedString, icon: String? = nil, shouldLoadOnTap: Bool = false, action: @escaping () -> Void) {
+    init(title: GDSLocalisedString, icon: ButtonIconViewModel? = nil, shouldLoadOnTap: Bool = false, action: @escaping () -> Void) {
         self.title = title
         self.icon = icon
         self.shouldLoadOnTap = shouldLoadOnTap
