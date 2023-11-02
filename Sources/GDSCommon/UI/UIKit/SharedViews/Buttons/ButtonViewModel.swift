20198f05109f41c3882ba9fc622a63e421631d38
Ben McKillop <ben.mckillop@digital.cabinet-office.gov.uk>
2023-11-02 15:02:26 +0000

    feat: moving vm into file of its own - comments

--- Sources/GDSCommon/UI/UIKit/SharedViews/Buttons/ButtonViewModel.swift
+++ Sources/GDSCommon/UI/UIKit/SharedViews/Buttons/ButtonViewModel.swift
@@ -6,12 +6,3 @@ public protocol ButtonViewModel {
     var shouldLoadOnTap: Bool { get }
     var action: () -> Void { get }
 }
-
-public protocol ButtonIconViewModel {
-    var iconName: String { get }
-    var symbolPosition: SymbolPosition { get }
-}
-
-public struct ButtonIcon {
-    public static let arrowUpRight: String = "arrow.up.right"
-}
