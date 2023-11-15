d269f06193befb7754013913ebf35872bd573cdc
Robin Phillips <rlphillips@deloitte.co.uk>
2023-11-15 15:44:18 +0000

    fix: removing redundant protocol requirements and docc

--- Sources/GDSCommon/UI/UIKit/SharedViews/Patterns/GDSInstructions/GDSInstructionsViewModel.swift
+++ Sources/GDSCommon/UI/UIKit/SharedViews/Patterns/GDSInstructions/GDSInstructionsViewModel.swift
@@ -10,21 +10,10 @@ import UIKit
 ///
 /// `childView` is added at the bottom of the `stackView` that contains the `title` and
 /// `body`. Because it is of type `UIView` the kind of view that can be added here is very flexible.
-///
-/// Additionally there is a protocol method `didAppear()` which should be used
-/// for any code that needs to be performed when the screen appears.
-/// For example, this might include tracking an analytics screen view, but it could be used
-/// for other code such as making an API call.
-/// If `rightBarButtonTitle` is `nil` then the navigation item right bar button is hidden
-/// Otherwise, it is set to the title property and the action is set to the `didDismiss()` action.
 public protocol GDSInstructionsViewModel {
     var title: GDSLocalisedString { get }
     var body: String { get }
-    var rightBarButtonTitle: GDSLocalisedString? { get }
     var childView: UIView { get }
     var buttonViewModel: ButtonViewModel { get }
     var secondaryButtonViewModel: ButtonViewModel? { get }
-    
-    func didAppear()
-    func didDismiss()
 }
