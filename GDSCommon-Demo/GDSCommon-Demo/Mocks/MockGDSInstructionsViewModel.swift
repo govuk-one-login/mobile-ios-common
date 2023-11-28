6405b665e403c52254a2a9977a3cb6ea15267eed
Robin Phillips <rlphillips@deloitte.co.uk>
2023-11-28 15:06:05 +0000

    linting issues

--- GDSCommon-Demo/GDSCommon-Demo/Mocks/MockGDSInstructionsViewModel.swift
+++ GDSCommon-Demo/GDSCommon-Demo/Mocks/MockGDSInstructionsViewModel.swift
@@ -6,9 +6,9 @@ struct MockGDSInstructionsViewModel: GDSInstructionsViewModel, BaseViewModel {
     var body: String = "We can add a subtitle here to give some extra context"
     var rightBarButtonTitle: GDSLocalisedString? = "right bar button"
     var childView: UIView = BulletView(title: "This is the bullet view",
-                                     text: ["Here we can list things we want the user to know",
-                                            "we can use this as a way to step them through an action",
-                                            "or give details of a process"])
+                                       text: ["Here we can list things we want the user to know",
+                                              "we can use this as a way to step them through an action",
+                                              "or give details of a process"])
     var buttonViewModel: ButtonViewModel
     var secondaryButtonViewModel: ButtonViewModel?
     var backButtonIsHidden: Bool = false
