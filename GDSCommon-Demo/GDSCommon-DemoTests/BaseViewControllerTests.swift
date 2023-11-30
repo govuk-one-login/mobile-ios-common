e8f309e2e6b417e6a81f4dff8cb5c83a48f1cbef
Robin Phillips <rlphillips@deloitte.co.uk>
2023-11-30 14:53:21 +0000

    chore: added tests as per PR comment

--- GDSCommon-Demo/GDSCommon-DemoTests/BaseViewControllerTests.swift
+++ GDSCommon-Demo/GDSCommon-DemoTests/BaseViewControllerTests.swift
@@ -5,13 +5,17 @@ import XCTest
 final class BaseViewControllerTests: XCTestCase {
     var viewModel: BaseViewModel!
     var sut: BaseViewController!
+    var didAppear: Bool!
+    var didDismiss: Bool!
     
     override func setUp() {
         super.setUp()
+        didAppear = false
+        didDismiss = false
         viewModel = MockBaseViewModel {
-            
+            self.didAppear = true
         } dismissAction: {
-            
+            self.didDismiss = true
         }
         sut = BaseViewController(viewModel: viewModel, nibName: "MockBase", bundle: .init(for: BaseViewControllerTests.self))
     }
@@ -31,5 +35,25 @@ extension BaseViewControllerTests {
         sut.endAppearanceTransition()
         let rightBarButton: UIBarButtonItem = try XCTUnwrap(sut.navigationItem.rightBarButtonItem)
         XCTAssertEqual(rightBarButton.title, "right bar button")
+    }
+    
+    func test_didAppear() {
+        XCTAssertFalse(didAppear)
+        sut.beginAppearanceTransition(true, animated: false)
+        sut.viewDidAppear(false)
+        sut.endAppearanceTransition()
+        XCTAssertTrue(didAppear)
+    }
+    
+    func test_didDismiss() {
+        XCTAssertFalse(didAppear)
+        sut.beginAppearanceTransition(true, animated: false)
+        sut.viewDidAppear(false)
+        sut.endAppearanceTransition()
+        XCTAssertTrue(didAppear)
+        
+        XCTAssertFalse(didDismiss)
+        _ = sut.navigationItem.rightBarButtonItem?.target?.perform(sut.navigationItem.rightBarButtonItem?.action)
+        XCTAssertTrue(didDismiss)
     }
 }
