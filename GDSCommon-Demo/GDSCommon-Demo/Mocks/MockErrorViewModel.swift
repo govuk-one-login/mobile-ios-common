7c1be5e81268c98f840f060364d34b9740592eca
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2023-12-06 10:45:31 +0000

    fix: reverting and including encasing views for the content stack view

--- GDSCommon-Demo/GDSCommon-Demo/Mocks/MockErrorViewModel.swift
+++ GDSCommon-Demo/GDSCommon-Demo/Mocks/MockErrorViewModel.swift
@@ -4,7 +4,7 @@ import UIKit
 struct MockErrorViewModel: ErrorViewModel, BaseViewModel {
     var image: UIImage = UIImage(systemName: "exclamationmark.circle") ?? UIImage()
     var title: GDSLocalisedString = "This is an Error View title"
-    var body: GDSLocalisedString = "This is an Error View body"
+    var body: GDSLocalisedString = "This is an Error View body This is an Error View body"
     var primaryButtonViewModel: ButtonViewModel = MockButtonViewModel.primary
     var secondaryButtonViewModel: ButtonViewModel? = MockButtonViewModel.secondary
     var rightBarButtonTitle: GDSLocalisedString?
