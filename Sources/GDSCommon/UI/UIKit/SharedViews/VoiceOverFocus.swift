4f6bbeb6a93d7843b49a65b0839edb984575a359
Robin Phillips <rlphillips@deloitte.co.uk>
2023-12-12 13:29:37 +0000

    fix: linting issue

--- Sources/GDSCommon/UI/UIKit/SharedViews/VoiceOverFocus.swift
+++ Sources/GDSCommon/UI/UIKit/SharedViews/VoiceOverFocus.swift
@@ -7,7 +7,8 @@ public protocol VoiceOverFocus {
     var initialVoiceOverView: UIView { get }
 }
 
-/// For screen view controllers within `GDSCommon` that have a ``titleLabel`` property and inherti from ``BaseViewController``, conform the view controller to ``TitledViewController`` to
+/// For screen view controllers within `GDSCommon` that have a ``titleLabel`` property and inherits
+/// from ``BaseViewController``, conform the view controller to ``TitledViewController`` to
 /// automaticaly direct VoiceOver to the ``titleLabel`` when the screen appears
 protocol TitledViewController: VoiceOverFocus {
     var titleLabel: UILabel! { get }
