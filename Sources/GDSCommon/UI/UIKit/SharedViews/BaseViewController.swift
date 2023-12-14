eec54ffcb91a94ca44e9f7ca7c515b831cf7fd2f
Robin Phillips <rlphillips@deloitte.co.uk>
2023-12-14 12:39:11 +0000

    fix: adjusting lifecycle for notification

--- Sources/GDSCommon/UI/UIKit/SharedViews/BaseViewController.swift
+++ Sources/GDSCommon/UI/UIKit/SharedViews/BaseViewController.swift
@@ -31,20 +31,34 @@ open class BaseViewController: UIViewController {
                                                            target: self,
                                                            action: #selector(dismissScreen))
         }
+        
+        UIAccessibility.post(notification: .screenChanged,
+                                 argument: nil)
+    }
+    
+    public override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        UIAccessibility.post(notification: .screenChanged,
+                                 argument: nil)
     }
     
     public override func viewIsAppearing(_ animated: Bool) {
         super.viewIsAppearing(animated)
         
-        if let screen = self as? VoiceOverFocus {
-            UIAccessibility.post(notification: .screenChanged,
-                                 argument: screen.initialVoiceOverView)
-        }
+//        if let screen = self as? VoiceOverFocus {
+//            UIAccessibility.post(notification: .screenChanged,
+//                                 argument: screen.initialVoiceOverView)
+//        }
     }
     
     public override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         viewModel?.didAppear()
+        
+        if let screen = self as? VoiceOverFocus {
+            UIAccessibility.post(notification: .screenChanged,
+                                 argument: screen.initialVoiceOverView)
+        }
     }
     
     @objc private func dismissScreen() {
