430942af5b2af127d4b7592357e1382d9ae8c199
Ben McKillop <ben.mckillop@digital.cabinet-office.gov.uk>
2024-01-11 15:17:55 +0000

    fix: Ensuring back button is set, welsh on scanning view

--- Sources/GDSCommon/UI/UIKit/SharedViews/Patterns/Scanner/ScanningViewController.swift
+++ Sources/GDSCommon/UI/UIKit/SharedViews/Patterns/Scanner/ScanningViewController.swift
@@ -71,6 +71,7 @@ public final class ScanningViewController<CaptureSession: GDSCommon.CaptureSessi
     
     public override func viewDidLoad() {
         super.viewDidLoad()
+        setBackButtonTitle()
         title = viewModel.title
     }
     
