84b0136e5970b563116c8945d38f3ff806e5f1b8
Ben McKillop <ben.mckillop@digital.cabinet-office.gov.uk>
2024-01-12 15:09:16 +0000

    fix: updating to working back button

--- Sources/GDSCommon/Extensions/UIKit/UIViewController+Extensions.swift
+++ Sources/GDSCommon/Extensions/UIKit/UIViewController+Extensions.swift
@@ -2,9 +2,10 @@ import UIKit
 
 extension UIViewController {
     func setBackButtonTitle(isHidden: Bool = false) {
-        let backButtonTitle = GDSLocalisedString(stringKey: "GDSCommon-backButtonTitle",
+        let backButtonTitle = GDSLocalisedString(stringKey: "GDSCommonBackButton",
                                                  bundle: .module)
-        navigationItem.backButtonTitle = backButtonTitle.value
+        let backButton = UIBarButtonItem(title: backButtonTitle.value, style: .plain, target: nil, action: nil)
+        navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
         navigationItem.hidesBackButton = isHidden
     }
 }
