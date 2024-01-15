e94f390e57977e76978274bd3cadec62cd14db78
Ben McKillop <ben.mckillop@digital.cabinet-office.gov.uk>
2024-01-15 09:56:10 +0000

    fix: reverting change

--- Sources/GDSCommon/Extensions/UIKit/UIViewController+Extensions.swift
+++ Sources/GDSCommon/Extensions/UIKit/UIViewController+Extensions.swift
@@ -4,8 +4,7 @@ extension UIViewController {
     func setBackButtonTitle(isHidden: Bool = false) {
         let backButtonTitle = GDSLocalisedString(stringKey: "GDSCommonBackButton",
                                                  bundle: .module)
-        let backButton = UIBarButtonItem(title: backButtonTitle.value, style: .plain, target: nil, action: nil)
-        navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
+        navigationItem.backButtonTitle = backButtonTitle.value
         navigationItem.hidesBackButton = isHidden
     }
 }
