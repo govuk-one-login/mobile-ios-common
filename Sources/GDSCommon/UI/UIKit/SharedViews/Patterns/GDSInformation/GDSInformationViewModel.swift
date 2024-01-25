b60d3a5a9b86024363d7cb4fcda9da0f3215643e
Shukri Ahmed <shukri.ahmed@digital.cabinet-office.gov.uk>
2024-01-25 15:03:35 +0000

    chore: changing lock image weight

--- Sources/GDSCommon/UI/UIKit/SharedViews/Patterns/GDSInformation/GDSInformationViewModel.swift
+++ Sources/GDSCommon/UI/UIKit/SharedViews/Patterns/GDSInformation/GDSInformationViewModel.swift
@@ -4,7 +4,7 @@ public protocol GDSInformationViewModel {
     var image: String { get }
     var title: GDSLocalisedString { get }
     var body: GDSLocalisedString { get }
-    var footnote: GDSLocalisedString? { get } // good name?
+    var footnote: GDSLocalisedString? { get }
     var primaryButtonViewModel: ButtonViewModel { get }
     var secondaryButtonViewModel: ButtonViewModel? { get }
 }
