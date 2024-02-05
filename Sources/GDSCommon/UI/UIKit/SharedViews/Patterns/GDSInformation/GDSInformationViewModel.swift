d45dd87f58b24d1dbaebaf75029b16282f3d9fa1
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2024-02-05 15:54:39 +0000

    docs: adding DocC to GDSInformationViewModel

--- Sources/GDSCommon/UI/UIKit/SharedViews/Patterns/GDSInformation/GDSInformationViewModel.swift
+++ Sources/GDSCommon/UI/UIKit/SharedViews/Patterns/GDSInformation/GDSInformationViewModel.swift
@@ -1,6 +1,25 @@
 import UIKit
 
-/// Protocol for the view model required to initilise ``GDSInformationViewController``
+/// View model for the ``GDSInformationViewController``
+/// - `image` type is `String`
+/// - `imageWeight` type is `UIFont.Weight`?
+/// - `imageColour` type is `UIColor`?
+/// - `imageHeightConstraint` type is `CGFloat`?
+/// - `title` type is ``GDSLocalisedString``
+/// - `body` type is ``GDSLocalisedString``?
+/// - `footnote` type is ``GDSLocalisedString``?
+/// - `primaryButtonViewModel` type is ``ButtonViewModel``
+/// - `secondaryButtonViewModel` type is ``ButtonViewModel``?
+///
+/// There are several optional properties as part of this protocol to extend the functionality
+/// of the ``GDSInformationViewController``. When properties are `nil`, default
+/// configuration has been created to account for the lack of specific configuration.
+/// Concrete implementations of this protocol can be combined with conformance to
+/// ``BaseViewModel`` to extend functionality to the parent class ``BaseViewController``.
+/// Functionality segregated onto ``BaseViewController`` pertains to optionally displaying
+/// screens modally and calling custom methods when screens appear and dismiss.
+/// For example, this might include tracking an analytics screen view, but it could be used
+/// for other code such as making an API call.
 public protocol GDSInformationViewModel {
     var image: String { get }
     var imageWeight: UIFont.Weight? { get }
