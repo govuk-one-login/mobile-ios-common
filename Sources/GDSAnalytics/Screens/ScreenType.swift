d44fbc60346dd4c6b8443a52e92f13f2c51f9a73
Ben McKillop <ben.mckillop@digital.cabinet-office.gov.uk>
2024-02-19 16:11:35 +0000

    fix: removing screen type

--- Sources/GDSAnalytics/Screens/ScreenType.swift
+++
@@ -1,7 +0,0 @@
-public enum ScreenType: String {
-    case modal
-    case splashFragment = "splashfragment"
-    case errorScreen = "errorscreen"
-    case viewController = "uicontroller"
-    case webView = "webview"
-}
--- Sources/GDSAnalytics/Screens/ScreenView.swift
+++ Sources/GDSAnalytics/Screens/ScreenView.swift
@@ -2,37 +2,21 @@ public protocol ScreenViewProtocol {
     associatedtype Screen: NamedScreen
     var screen: Screen { get }
     var title: String { get }
-    var type: ScreenType? { get }
-    var id: String? { get }
     var parameters: [String: String] { get }
 }
 
 public struct ScreenView<Screen: NamedScreen>: ScreenViewProtocol {
     public let screen: Screen
     public let title: String
-    public var type: ScreenType?
-    public let id: String?
     
     public var parameters: [String: String] {
-        if let id, let type {
-            return [ScreenParameter.id.rawValue: id,
-                    ScreenParameter.title.rawValue: title,
-                    ScreenParameter.type.rawValue: type.rawValue]
-                .mapValues(\.formattedAsParameter)
-        } else {
-            return [ScreenParameter.title.rawValue: title]
-                .mapValues(\.formattedAsParameter)
-        }
+        [:]
     }
     
     public init(screen: Screen,
-                id: String?,
-                type: ScreenType?,
                 titleKey: String,
                 variableKeys: [String] = []) {
         self.screen = screen
         self.title = titleKey.englishString(variableKeys)
-        self.id = id
-        self.type = type
     }
 }
