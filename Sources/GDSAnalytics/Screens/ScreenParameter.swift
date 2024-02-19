0d05e0e782f8690eae734ddee0473513cbf71eb0
Ben McKillop <ben.mckillop@digital.cabinet-office.gov.uk>
2024-02-19 15:24:52 +0000

    fix: renaming parameters

--- Sources/GDSAnalytics/Screens/ScreenParameter.swift
+++ Sources/GDSAnalytics/Screens/ScreenParameter.swift
@@ -3,7 +3,7 @@ public enum ScreenParameter: String {
     case endpoint
     case status
     case hash
-    case title
+    case title = "screen_name"
     case id = "screen_id"
-    case type
+    case type = "screen_class"
 }
