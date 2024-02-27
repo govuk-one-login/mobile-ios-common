c43331c0dddeb434bf49d2119449463f46234c87
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2024-02-27 17:04:15 +0000

    fix: giving mock a body text colour value

--- GDSCommon-Demo/GDSCommon-Demo/Mocks/MockModalInfoViewModel.swift
+++ GDSCommon-Demo/GDSCommon-Demo/Mocks/MockModalInfoViewModel.swift
@@ -5,7 +5,7 @@ import UIKit
 struct MockModalInfoViewModel: ModalInfoViewModel, BaseViewModel {
     let title: GDSLocalisedString = "This is the modal view"
     let body: GDSLocalisedString = "We can use this if we want the user to complete an action"
-    var bodyTextColour: UIColor? = nil
+    var bodyTextColour: UIColor? =
     var primaryButtonViewModel: ButtonViewModel? = MockButtonViewModel.primary
     var secondaryButtonViewModel: ButtonViewModel? = MockButtonViewModel.secondary
     let rightBarButtonTitle: GDSLocalisedString? = "Close"
