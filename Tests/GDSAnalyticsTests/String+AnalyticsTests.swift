729727ac4f440078d9fbc2675d8ce6d88f6f13ab
Ben McKillop <ben.mckillop@digital.cabinet-office.gov.uk>
2024-03-05 10:32:14 +0000

    tests: adding tests for EnglishString

--- Tests/GDSAnalyticsTests/String+AnalyticsTests.swift
+++ Tests/GDSAnalyticsTests/String+AnalyticsTests.swift
@@ -13,4 +13,17 @@ final class StringExtensionTests: XCTestCase {
         XCTAssertEqual(test.formattedAsParameter,
                        "this is a string that is more than 100 characters. this is a string that is more than 100 characters")
     }
+    
+    func testEnglishString_findsBundle() {
+        let test = "GDSCommonTestString"
+        let englishString = test.englishString(bundle: .module)
+        XCTAssertEqual(englishString, "Test")
+    }
+    
+    func testEnglishString_doesntFindBundle() {
+        let test = "GDSCommonTestString"
+        let englishString = test.englishString(bundle: .main)
+        XCTAssertNotEqual(englishString, "Test")
+    }
+
 }
