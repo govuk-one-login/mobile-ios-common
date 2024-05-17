1f19cbc16f791a5e42afe1b83d9550034d5f38b6
Joshua Dubey <josh.dubey@digital.cabinet-office.gov.uk>
2024-05-17 14:53:04 +0100

    chore:  mark required init: unavailable, so it is omitted from code coverage requirements

--- Sources/GDSCommon/UI/UIKit/SharedViews/Patterns/GDSLoadingScreen/GDSLoadingViewController.swift
+++ Sources/GDSCommon/UI/UIKit/SharedViews/Patterns/GDSLoadingScreen/GDSLoadingViewController.swift
@@ -14,6 +14,7 @@ public final class GDSLoadingViewController: BaseViewController {
         super.init(viewModel: viewModel as? BaseViewModel, nibName: "GDSLoading", bundle: .module)
     }
 
+    @available(*, unavailable, renamed: "init(viewModel:)")
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
