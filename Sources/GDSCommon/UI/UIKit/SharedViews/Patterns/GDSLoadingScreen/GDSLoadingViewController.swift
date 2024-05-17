e4921719dbc06c3109565c19924de3e0b7e0d585
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2024-05-17 14:06:34 +0100

    docs: adding basic documentation

--- Sources/GDSCommon/UI/UIKit/SharedViews/Patterns/GDSLoadingScreen/GDSLoadingViewController.swift
+++ Sources/GDSCommon/UI/UIKit/SharedViews/Patterns/GDSLoadingScreen/GDSLoadingViewController.swift
@@ -1,6 +1,10 @@
 import Foundation
 import UIKit
 
+/// View controller for `GDSLoading` screen.
+///   - `loadingLabel` (type: `UILabel`)
+/// It is initialised with a `viewModel` of type: `GDSLoadingViewModel`
+/// This screen is typically used to as a loading screen for asynchronous tasks to inform the user and prevent interaction with the app UI.
 public final class GDSLoadingViewController: BaseViewController {
     public override var nibName: String? { "GDSLoading" }
     private let viewModel: GDSLoadingViewModel
--- Sources/GDSCommon/UI/UIKit/SharedViews/Patterns/GDSLoadingScreen/GDSLoadingViewModel.swift
+++ Sources/GDSCommon/UI/UIKit/SharedViews/Patterns/GDSLoadingScreen/GDSLoadingViewModel.swift
@@ -1,5 +1,12 @@
 import Foundation
 
+/// Protocol for the view model required to initilise a ``GDSLoadingViewController``
+/// Concrete implementations of this protocol can be combined with conformance to
+/// ``BaseViewModel`` to extend functionality to the parent class ``BaseViewController``.
+/// Functionality segregated onto ``BaseViewController`` pertains to optionally displaying
+/// screens modally and calling custom methods when screens appear and dismiss.
+/// For example, this might include tracking an analytics screen view, but it could be used
+/// for other code such as making an API call.
 public protocol GDSLoadingViewModel {
     var loadingLabelKey: GDSLocalisedString { get }
 }
