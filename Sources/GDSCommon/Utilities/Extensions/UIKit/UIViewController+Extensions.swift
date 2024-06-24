b7b7633b4e9ad9bc947798d2f46dd7d199d09243
Ben McKillop <ben.mckillop@digital.cabinet-office.gov.uk>
2024-06-24 15:46:26 +0100

    fix: testing

--- Sources/GDSCommon/Utilities/Extensions/UIKit/UIViewController+Extensions.swift
+++ Sources/GDSCommon/Utilities/Extensions/UIKit/UIViewController+Extensions.swift
@@ -2,6 +2,9 @@ import UIKit
 
 extension UIViewController {
     func setBackButtonTitle(isHidden: Bool = false) {
+        
+        print(NSLocalizedString(key: "GDSCommonBackButton", bundle: .module))
+        print(isHidden)
         navigationItem.backButtonTitle = NSLocalizedString(key: "GDSCommonBackButton", bundle: .module)
         navigationItem.hidesBackButton = isHidden
     }
