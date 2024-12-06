346d90a761c880ae96684d166550a1211c7daef8
shukriahmed-dd <shukri.ahmed@digital.cabinet-office.gov.uk>
2024-12-06 16:28:47 +0000

    chore: updating mocks

--- GDSCommon-Demo/GDSCommon-Demo/Mocks/MockGDSInformationViewModel.swift
+++ GDSCommon-Demo/GDSCommon-Demo/Mocks/MockGDSInformationViewModel.swift
@@ -1,32 +1,13 @@
 import GDSCommon
 import UIKit
 
-struct MockGDSInformationViewModelPlain: GDSInformationViewModelV2,
-                                         GDSInformationViewModelWithOptionalPrimaryButton,
-                                         BaseViewModel {
-    let image: String = "lock"
-    let imageWeight: UIFont.Weight? = nil
-    let imageColour: UIColor? = nil
-    let imageHeightConstraint: CGFloat? = nil
-    let title: GDSLocalisedString = "This is an Information View title"
-    let body: GDSLocalisedString? = "This is an (optional) Information View body."
-    let footnote: GDSLocalisedString? = nil
-    let primaryButtonViewModel: ButtonViewModel? = nil
-    let secondaryButtonViewModel: ButtonViewModel? = nil
-    let rightBarButtonTitle: GDSLocalisedString? = nil
-    let backButtonIsHidden: Bool = false
-    
-    func didAppear() {}
-    
-    func didDismiss() {}
-}
-
+// Confirming to the original deprecated protocol
 struct MockGDSInformationViewModel: GDSInformationViewModel,
                                     BaseViewModel {
     let image: String = "lock"
     let imageWeight: UIFont.Weight? = nil
     let imageColour: UIColor? = nil
-    let imageHeightConstraint: CGFloat? = nil
+    let imageHeightConstraint: CGFloat = 55 // not optional anymore
     let title: GDSLocalisedString = "This is an Information View title"
     let body: GDSLocalisedString? = "This is an (optional) Information View body."
     let footnote: GDSLocalisedString? = "This is an (optional) Information View footnote where additional information for the buttons can be detailed."
@@ -40,18 +21,66 @@ struct MockGDSInformationViewModel: GDSInformationViewModel,
     func didDismiss() {}
 }
 
-struct MockGDSInformationViewModelWithChildView: GDSInformationViewModel,
-                                                 GDSInformationViewModelWithChildView,
-                                                 BaseViewModel {
+// Conforming to deprecated V2 protocols
+struct MockGDSInformationViewModelV2: GDSInformationViewModelV2,
+                                      GDSInformationViewModelWithFootnote,
+                                      GDSInformationViewModelPrimaryButton,
+                                      GDSInformationViewModelWithSecondaryButton,
+                                      GDSInformationViewModelWithChildView,
+                                      BaseViewModel {
+    let title: GDSLocalisedString = "This is an Information View title"
+    let body: GDSLocalisedString? = "This is an (optional) Information View body."
+    let image: String = "lock"
+    let imageHeightConstraint: CGFloat = 44 // not optional anymore
+    let imageWeight: UIFont.Weight? = nil
+    let imageColour: UIColor? = nil
+    let footnote: GDSLocalisedString = "This is an (optional) Information View footnote where additional information for the buttons can be detailed."
+    let primaryButtonViewModel: ButtonViewModel = MockButtonViewModel.primary
+    let secondaryButtonViewModel: ButtonViewModel = MockButtonViewModel.secondary
+    let rightBarButtonTitle: GDSLocalisedString? = nil
+    let backButtonIsHidden: Bool = false
+    
+    var childView: UIView {
+        createChildView()
+    }
+     
+    func didAppear() { }
+    
+    func didDismiss() { }
+    
+    private func createChildView() -> UIView {
+        let body = UILabel()
+        body.text = GDSLocalisedString(stringLiteral: "This is a child view which can be populated with text or components").value
+        body.adjustsFontForContentSizeCategory = true
+        body.font = .body
+        body.numberOfLines = 0
+        body.textAlignment = .center
+        let stackView = UIStackView(arrangedSubviews: [body])
+        stackView.axis = .vertical
+        stackView.alignment = .top
+        stackView.alignment = .fill
+        stackView.spacing = 16
+        return stackView
+    }
+}
+
+// Conforming to new protocol to that replaces the deprecated protocols
+struct MockGDSCentreAlignedViewModel: GDSCentreAlignedViewModel,
+                                      GDSCentreAlignedViewModelWithImage,
+                                      GDSCentreAlignedViewModelWithFootnote,
+                                      GDSCentreAlignedViewModelWithChildView,
+                                      GDSCentreAlignedViewModelWithPrimaryButton,
+                                      GDSCentreAlignedViewModelWithSecondaryButton,
+                                      BaseViewModel {
     let image: String = "lock"
     let imageWeight: UIFont.Weight? = nil
     let imageColour: UIColor? = nil
-    let imageHeightConstraint: CGFloat? = nil
+    let imageHeightConstraint: CGFloat = 77
     let title: GDSLocalisedString = "This is an Information View title"
     let body: GDSLocalisedString? = "This is an (optional) Information View body."
-    let footnote: GDSLocalisedString? = "This is an (optional) Information View footnote where additional information for the buttons can be detailed."
+    let footnote: GDSLocalisedString = "This is an (optional) Information View footnote where additional information for the buttons can be detailed."
     let primaryButtonViewModel: ButtonViewModel = MockButtonViewModel.primary
-    let secondaryButtonViewModel: ButtonViewModel? = MockButtonViewModel.secondary
+    let secondaryButtonViewModel: ButtonViewModel = MockButtonViewModel.secondary
     let rightBarButtonTitle: GDSLocalisedString? = nil
     let backButtonIsHidden: Bool = false
 
@@ -64,16 +93,12 @@ struct MockGDSInformationViewModelWithChildView: GDSInformationViewModel,
     }
 
     private func createChildView() -> UIView {
-        let bulletView = BulletView(viewModel: MockBulletViewModel(title: "This means", titleFont: .body))
-        let body = UILabel()
-        body.text = GDSLocalisedString(stringLiteral: "More (optional) text can follow, too").value
-        body.adjustsFontForContentSizeCategory = true
-        body.font = .body
-        body.numberOfLines = 0
-        let stackView = UIStackView(arrangedSubviews: [bulletView, body])
+        let bulletView = BulletView(viewModel: MockBulletViewModel(title: "Optional child view - populated with a BulletView", titleFont: .body))
+        let stackView = UIStackView(arrangedSubviews: [bulletView])
         stackView.axis = .vertical
         stackView.alignment = .top
-        stackView.spacing = 12
+        stackView.alignment = .fill
+        stackView.spacing = 16
         return stackView
     }
 }
