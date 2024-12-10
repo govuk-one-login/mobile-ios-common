ef111f757f8ab4cc0fa5640cb66aeccb69aded41
Ben McKillop <ben.mckillop@digital.cabinet-office.gov.uk>
2024-12-10 10:42:35 +0000

    fix: updating ViewInspector version

--- Tests/GDSCommonTests/ComponentTests/Text+ExtensionsTests.swift
+++ Tests/GDSCommonTests/ComponentTests/Text+ExtensionsTests.swift
@@ -6,37 +6,37 @@ final class TextExtensionsTests: XCTestCase {}
 
 extension TextExtensionsTests {
     func test_titleStyle() throws {
-        XCTAssertEqual(try Text("").title().inspect().text().attributes().font(), Font.largeTitle.weight(.bold))
-        XCTAssertEqual(try Text("").title().inspect().multilineTextAlignment(), .center)
-        XCTAssertEqual(try Text("").title().inspect().fixedSize().horizontal, false)
-        XCTAssertEqual(try Text("").title().inspect().fixedSize().vertical, true)
+        XCTAssertEqual(try Text("").title().inspect().implicitAnyView().text().attributes().font(), Font.largeTitle.weight(.bold))
+        XCTAssertEqual(try Text("").title().inspect().implicitAnyView().text().multilineTextAlignment(), .center)
+        XCTAssertEqual(try Text("").title().inspect().implicitAnyView().text().fixedSize().horizontal, false)
+        XCTAssertEqual(try Text("").title().inspect().implicitAnyView().text().fixedSize().vertical, true)
     }
     
     func test_subtitleStyle() throws {
         if #available(iOS 14.0, *) {
-            XCTAssertEqual(try Text("").subtitle().inspect().text().attributes().font(), Font.title3.weight(.bold))
+            XCTAssertEqual(try Text("").subtitle().inspect().implicitAnyView().text().attributes().font(), Font.title3.weight(.bold))
         } else {
-            XCTAssertEqual(try Text("").subtitle().inspect().text().attributes().font(), Font.system(size: UIFontMetrics.default.scaledValue(for: 20)))
-            XCTAssertEqual(try Text("").subtitle().inspect().text().attributes().fontWeight(), Font.Weight.bold)
+            XCTAssertEqual(try Text("").subtitle().inspect().implicitAnyView().text().attributes().font(), Font.system(size: UIFontMetrics.default.scaledValue(for: 20)))
+            XCTAssertEqual(try Text("").subtitle().inspect().implicitAnyView().text().attributes().fontWeight(), Font.Weight.bold)
         }
     }
     
     func test_bodyCentreAlignedTextStyle() throws {
-        XCTAssertEqual(try Text("").bodyCentreAligned().inspect().text().attributes().font(), Font.body)
-        XCTAssertEqual(try Text("").bodyCentreAligned().inspect().multilineTextAlignment(), .center)
-        XCTAssertEqual(try Text("").bodyCentreAligned().inspect().flexFrame().maxWidth, .infinity)
+        XCTAssertEqual(try Text("").bodyCentreAligned().inspect().implicitAnyView().text().attributes().font(), Font.body)
+        XCTAssertEqual(try Text("").bodyCentreAligned().inspect().implicitAnyView().text().multilineTextAlignment(), .center)
+        XCTAssertEqual(try Text("").bodyCentreAligned().inspect().implicitAnyView().text().flexFrame().maxWidth, .infinity)
     }
     
     func test_bodyHeaderTextStyle() throws {
-        XCTAssertEqual(try Text("").bodyHeader().inspect().text().attributes().isBold(), true)
-        XCTAssertEqual(try Text("").bodyHeader().inspect().fixedSize().horizontal, false)
-        XCTAssertEqual(try Text("").bodyHeader().inspect().fixedSize().vertical, true)
+        XCTAssertEqual(try Text("").bodyHeader().inspect().implicitAnyView().text().attributes().isBold(), true)
+        XCTAssertEqual(try Text("").bodyHeader().inspect().implicitAnyView().text().fixedSize().horizontal, false)
+        XCTAssertEqual(try Text("").bodyHeader().inspect().implicitAnyView().text().fixedSize().vertical, true)
     }
     
     func test_calloutBoldTextStyle() throws {
-        XCTAssertEqual(try Text("").calloutBold().inspect().text().attributes().isBold(), true)
-        XCTAssertEqual(try Text("").calloutBold().inspect().fixedSize().horizontal, false)
-        XCTAssertEqual(try Text("").calloutBold().inspect().fixedSize().vertical, true)
-        XCTAssertEqual(try Text("").calloutBold().inspect().text().attributes().font(), .callout)
+        XCTAssertEqual(try Text("").calloutBold().inspect().implicitAnyView().text().attributes().isBold(), true)
+        XCTAssertEqual(try Text("").calloutBold().inspect().implicitAnyView().text().fixedSize().horizontal, false)
+        XCTAssertEqual(try Text("").calloutBold().inspect().implicitAnyView().text().fixedSize().vertical, true)
+        XCTAssertEqual(try Text("").calloutBold().inspect().implicitAnyView().text().attributes().font(), .callout)
     }
 }
