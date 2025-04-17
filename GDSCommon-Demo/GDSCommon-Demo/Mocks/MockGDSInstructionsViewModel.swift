16b88ce0cce9f75faf07e03697d5952c90337be0
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2025-04-17 10:10:31 +0100

    chore: cleaning up

--- GDSCommon-Demo/GDSCommon-Demo/Mocks/MockGDSInstructionsViewModel.swift
+++ GDSCommon-Demo/GDSCommon-Demo/Mocks/MockGDSInstructionsViewModel.swift
@@ -5,21 +5,7 @@ struct MockGDSInstructionsViewModel: GDSInstructionsViewModel, BaseViewModel {
     let title: GDSLocalisedString = "This is the Instructions View"
     let body: String = "We can add a subtitle here to give some extra context"
     let rightBarButtonTitle: GDSLocalisedString? = "right bar button"
-    let childView: UIView = NumberedListView(
-        viewModel: MockNumberedListViewModel(
-            title: "Test numbered list view",
-            titleFont: .body,
-            listItemStrings: [
-                "first numbered list element",
-                .init(
-                    stringLiteral: "second numbered list element",
-                    attributes: [("numbered list", [.font: UIFont.bodyBold])]
-                ),
-                "third numbered list element",
-                "fourth numbered list element"
-            ]
-        )
-    )
+    let childView: UIView = NumberedListView(viewModel: MockNumberedListViewModel())
     let buttonViewModel: ButtonViewModel
     let secondaryButtonViewModel: ButtonViewModel?
     let backButtonIsHidden: Bool = false
--- GDSCommon-Demo/GDSCommon-Demo/Mocks/MockNumberedListViewModel.swift
+++ GDSCommon-Demo/GDSCommon-Demo/Mocks/MockNumberedListViewModel.swift
@@ -2,7 +2,15 @@ import GDSCommon
 import UIKit
 
 struct MockNumberedListViewModel: NumberedListViewModel {
-    var title: GDSLocalisedString
-    var titleFont: UIFont?
-    var listItemStrings: [GDSLocalisedString]
+    var title: GDSLocalisedString = "Test numbered list view"
+    var titleFont: UIFont? = .body
+    var listItemStrings: [GDSLocalisedString] = [
+        "first numbered list element",
+        GDSLocalisedString(
+            stringLiteral: "second numbered list element",
+            attributes: [("numbered list", [.font: UIFont.bodyBold])]
+        ),
+        "third numbered list element",
+        "fourth numbered list element"
+    ]
 }
