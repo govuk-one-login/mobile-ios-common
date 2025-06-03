d9744bd2270b3d67f0dc80904baa3ceea9304069
Joe McGarry <joe.mcgarry@digital.cabinet-office.gov.uk>
2025-06-03 12:49:00 +0100

    refactor: reverts to depreciated view model approach

--- Sources/GDSCommon/Patterns/GDSListOptions/GDSListOptionsViewModel.swift
+++ Sources/GDSCommon/Patterns/GDSListOptions/GDSListOptionsViewModel.swift
@@ -2,6 +2,7 @@ import Foundation
 import UIKit
 
 /// Protocol for the view model required to initilise a ``ListOptionsViewController``
+@available(*, deprecated, renamed: "GDSListOptionsViewModelV2", message: "Please use GDSListOptionsViewModelV2 for accessibility enhancements") // depreciated 3/6/2025
 @MainActor
 public protocol GDSListOptionsViewModel {
     var title: GDSLocalisedString { get }
@@ -15,9 +16,26 @@ public protocol GDSListOptionsViewModel {
     var resultAction: (GDSLocalisedString) -> Void { get }
 }
 
-/// Protocol for the view model with accessibility enhancements for ``GDSListOptionsViewController``
+/// Protocol for the view model to initialise ``GDSListOptionsViewController`` with TableView Cell accessibility enhancements
+/// `accessibilityTraits` sets `cell.accessibiltyTraits` and tells VoiceOver if it is a button, link
+///    - note: if the option can be selected, the trait will also be set to `.selected` and announced.
+///    - As of iOS18, the unselected state is not announced.
+/// `accessibilityLabel` sets `cell.accessibilityLabel`. This is usually the value you have in the cell
+/// `accessibilityHint` sets `cell.accessibilityHint`. This is extra content if the label is too long
+///     - i.e "option 1 of 3"
+/// Order of VoiceOver announcing accessibility seems to be:
+/// - "`accessibiltyLabel`, `accessibilityTraits`, `accessibilityHint`"
 @MainActor
-public protocol GDSListOptionsViewModelAccessibility  {
+public protocol GDSListOptionsViewModelV2  {
+    var title: GDSLocalisedString { get }
+    var body: GDSLocalisedString? { get }
+    var childView: UIView? { get }
+    var listTitle: GDSLocalisedString? { get }
+    var listRows: [GDSLocalisedString] { get }
+    var listFooter: GDSLocalisedString? { get }
+    var buttonViewModel: ButtonViewModel { get }
+    var secondaryButtonViewModel: ButtonViewModel? { get }
+    var resultAction: (GDSLocalisedString) -> Void { get }
     var accessibilityLabel: [String] { get }
     var accessibilityHint: [String] { get }
     var accessibilityTraits: UIAccessibilityTraits { get }
