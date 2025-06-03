64aa3030e5a6b9e639d5d5ea3228dead6386d874
Joe McGarry <joe.mcgarry@digital.cabinet-office.gov.uk>
2025-06-03 07:57:28 +0100

    refactor: changes V2 view model

--- Sources/GDSCommon/Patterns/GDSListOptions/GDSListOptionsViewModel.swift
+++ Sources/GDSCommon/Patterns/GDSListOptions/GDSListOptionsViewModel.swift
@@ -2,7 +2,6 @@ import Foundation
 import UIKit
 
 /// Protocol for the view model required to initilise a ``ListOptionsViewController``
-@available(*, deprecated, renamed: "GDSListOptionsViewModelV2", message: "Updates to accessibility for VoiceOver with list options have been made. Please use GDSListOptionsViewModelV2") // depreciated 29/4/25
 @MainActor
 public protocol GDSListOptionsViewModel {
     var title: GDSLocalisedString { get }
@@ -18,17 +17,8 @@ public protocol GDSListOptionsViewModel {
 
 /// Protocol for the view model with accessibility enhancements for ``GDSListOptionsViewController``
 @MainActor
-public protocol GDSListOptionsViewModelV2  {
+public protocol GDSListOptionsViewModelAccessibility  {
     var accessibilityLabel: [String] { get }
     var accessibilityHint: [String] { get }
     var accessibilityTraits: UIAccessibilityTraits { get }
-    var title: GDSLocalisedString { get }
-    var body: GDSLocalisedString? { get }
-    var childView: UIView? { get }
-    var listTitle: GDSLocalisedString? { get }
-    var listRows: [GDSLocalisedString] { get }
-    var listFooter: GDSLocalisedString? { get }
-    var buttonViewModel: ButtonViewModel { get }
-    var secondaryButtonViewModel: ButtonViewModel? { get }
-    var resultAction: (GDSLocalisedString) -> Void { get }
 }
