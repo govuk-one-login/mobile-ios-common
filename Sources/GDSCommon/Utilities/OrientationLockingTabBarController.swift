b2d84432b846ceb60198b433018e563d46f7a456
shukriahmed-dd <shukri.ahmed@digital.cabinet-office.gov.uk>
2025-07-02 12:14:19 +0100

    feat: tabBarController orientation locking functionality  (#148)
    * feat: created OrientationLockingTabBarController
    
    * chore: change to navigationController
    
    * chore: make tabBar public
    
    * chore: change to viewController
    
    * chore: fix tests
    
    * chore: update naming
    
    * chore: change type to navigation controller

---
+++ Sources/GDSCommon/Utilities/OrientationLockingTabBarController.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+public protocol OrientationLockingNavigationController: UINavigationController {
+    // Empty implementation
+}
+
+// OrientationLockingTabBarController prevents interface rotation preferences being overridden by UITabBarController.
+// It ensures the correct view controller handles rotation.
+public class OrientationLockingTabBarController: UITabBarController {
+    override open var shouldAutorotate: Bool {
+        if let vc = self.selectedViewController?.presentedViewController, vc is OrientationLockingNavigationController {
+            return vc.shouldAutorotate
+        } else {
+            return super.shouldAutorotate
+        }
+    }
+    
+    override open var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation {
+        if let vc = self.selectedViewController?.presentedViewController, vc is OrientationLockingNavigationController {
+            return vc.preferredInterfaceOrientationForPresentation
+        } else {
+            return super.preferredInterfaceOrientationForPresentation
+        }
+    }
+    
+    override open var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if let vc = self.selectedViewController?.presentedViewController, vc is OrientationLockingNavigationController {
+            return vc.supportedInterfaceOrientations
+        } else {
+            return super.supportedInterfaceOrientations
+        }
+    }
+    
+    override open var preferredStatusBarStyle: UIStatusBarStyle {
+        if let vc = self.selectedViewController?.presentedViewController, vc is OrientationLockingNavigationController {
+            return vc.preferredStatusBarStyle
+        } else {
+            return super.preferredStatusBarStyle
+        }
+    }
+}
---
+++ Tests/GDSCommonTests/OrientationLockingTabBarControllerTests.swift
@@ -0,0 +1,152 @@
+@testable import GDSCommon
+import XCTest
+
+final class OrientationLockingTabBarControllerTests: XCTestCase {
+    private var sut: OrientationLockingTabBarController!
+    private var navigationController: UINavigationController!
+    private var orientationLockingViewController: OrientationLockingNavigationController!
+    private var viewController: UIViewController!
+    
+    override func setUp() {
+        super.setUp()
+        sut = OrientationLockingTabBarController()
+        navigationController = UINavigationController()
+        orientationLockingViewController = MockOrientationLockingViewController()
+        viewController = UIViewController()
+        
+        sut.setViewControllers([navigationController], animated: true)
+        attachToWindow(viewController: sut)
+    }
+    
+    override func tearDown() {
+        orientationLockingViewController = nil
+        viewController = nil
+        sut = nil
+        super.tearDown()
+    }
+}
+
+// Result of OrientationLockingTabBarController should match OrientationLockingViewController
+extension OrientationLockingTabBarControllerTests {
+    func test_shouldNotAutorotate() {
+        sut.selectedViewController = navigationController
+        orientationLockingViewController = MockOrientationLockingViewController(shouldAutorotate: false)
+        
+        // Present OrientationLockingViewController modally
+        navigationController.present(orientationLockingViewController, animated: false)
+        
+        // TabBarController rotation should match MockOrientationLockingViewController rotation
+        XCTAssertEqual(sut.shouldAutorotate, false)
+    }
+    
+    func test_shouldAutorotate() {
+        sut.selectedViewController = navigationController
+        orientationLockingViewController = MockOrientationLockingViewController(shouldAutorotate: true)
+        
+        // Present OrientationLockingViewController modally
+        navigationController.present(orientationLockingViewController, animated: true)
+        
+        // TabBarController rotation should match MockOrientationLockingViewController rotation
+        XCTAssertEqual(sut.shouldAutorotate, true)
+    }
+    
+    func test_preferredInterfaceOrientationForPresentation() {
+        sut.selectedViewController = navigationController
+        
+        // Present OrientationLockingViewController modally
+        navigationController.present(orientationLockingViewController, animated: false)
+        
+        // TabBarController rotation should match MockOrientationLockingViewController rotation
+        XCTAssertEqual(sut.preferredInterfaceOrientationForPresentation, .landscapeLeft)
+    }
+    
+    func test_supportedInterfaceOrientations() {
+        sut.selectedViewController = navigationController
+        
+        // Present OrientationLockingViewController modally
+        navigationController.present(orientationLockingViewController, animated: false)
+        
+        // TabBarController rotation should match MockOrientationLockingViewController rotation
+        XCTAssertEqual(sut.supportedInterfaceOrientations, .landscapeLeft)
+    }
+    
+    func test_preferredStatusBarStyle() {
+        sut.selectedViewController = navigationController
+        
+        // Present OrientationLockingViewController modally
+        navigationController.present(orientationLockingViewController, animated: false)
+        
+        // TabBarController rotation should match MockOrientationLockingViewController rotation
+        XCTAssertEqual(sut.preferredStatusBarStyle, .darkContent)
+    }
+}
+
+// Result of OrientationLockingTabBarController should match default behaviour of UITabBarController
+extension OrientationLockingTabBarControllerTests {
+    func test_default_shouldAutorotate() {
+        sut.selectedViewController = navigationController
+        
+        // Present a UIViewController modally
+        navigationController.present(viewController, animated: false)
+        
+        XCTAssertEqual(sut.shouldAutorotate, UITabBarController().shouldAutorotate)
+    }
+    
+    func test_default_preferredInterfaceOrientationForPresentation() {
+        sut.selectedViewController = navigationController
+        
+        // Present a UIViewController modally
+        navigationController.present(viewController, animated: false)
+        
+        // Since the UIViewController is being presented modally
+        // This value value will not be set and will be .unknown
+        XCTAssertEqual(sut.preferredInterfaceOrientationForPresentation, .unknown)
+    }
+    
+    func test_default_supportedInterfaceOrientations() {
+        sut.selectedViewController = navigationController
+        
+        // Present a UIViewController modally
+        navigationController.present(viewController, animated: false)
+        
+        XCTAssertEqual(sut.supportedInterfaceOrientations, UITabBarController().supportedInterfaceOrientations)
+    }
+    
+    func test_default_preferredStatusBarStyle() {
+        sut.selectedViewController = navigationController
+        
+        // Present a UIViewController modally
+        navigationController.present(viewController, animated: false)
+        
+        XCTAssertEqual(sut.preferredStatusBarStyle, UITabBarController().preferredStatusBarStyle)
+    }
+}
+
+class MockOrientationLockingViewController: UINavigationController, OrientationLockingNavigationController {
+    let shouldAutorotateValue: Bool
+    
+    init(shouldAutorotate: Bool = false) {
+        shouldAutorotateValue = shouldAutorotate
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override var shouldAutorotate: Bool {
+        shouldAutorotateValue
+    }
+    
+    override var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation {
+        .landscapeLeft
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        .landscapeLeft
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        .darkContent
+    }
+}
