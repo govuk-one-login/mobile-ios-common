de160e1d89453ab7b4714fd04e7f44cb9c9e3de2
Robin Phillips <robin.phillips1@digital.cabinet-office.gov.uk>
2026-06-16 20:12:41 +0100

    added un-committed file

---
+++ Sources/GDSCommon/Patterns/Scanner/ScanningViewController+CaptureSetup.swift
@@ -0,0 +1,48 @@
+import AVFoundation
+import UIKit
+
+extension ScanningViewController {
+    func setupVideoDisplay() {
+        guard let videoCaptureDevice = captureDevice.for(mediaType: .video),
+              let videoInput = try? videoCaptureDevice.input as? CaptureSession.Input else {
+            errorHandler(nil)
+            return
+        }
+        guard captureSession.canAddInput(videoInput) else {
+            assertionFailure("Can't add video input for detecting barcodes")
+            errorHandler(nil)
+            return
+        }
+        captureSession.addInput(videoInput)
+    }
+
+    func setupMetadataCapture() {
+        let output = AVCaptureVideoDataOutput()
+        guard captureSession.canAddOutput(output) else {
+            assertionFailure("Can't add video output for detecting barcodes")
+            errorHandler(nil)
+            return
+        }
+        output.alwaysDiscardsLateVideoFrames = true
+        output.setSampleBufferDelegate(self, queue: processingQueue)
+
+        captureSession.addOutput(output)
+
+        videoDataOutput = output
+    }
+
+    func updatePreviewLayerFrame() {
+        let safeAreaFrame = view.safeAreaLayoutGuide.layoutFrame
+        let convertedFrame = cameraView.convert(safeAreaFrame, from: view)
+        previewLayer.frame = convertedFrame
+    }
+
+    func setupInstructionLabel() {
+        view.addSubview(instructionsLabel)
+        NSLayoutConstraint.activate([
+            instructionsLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 26),
+            instructionsLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            instructionsLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
+        ])
+    }
+}
